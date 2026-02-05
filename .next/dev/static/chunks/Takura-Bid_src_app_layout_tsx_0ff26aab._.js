571a79a96b29b3b70be3d8cdf86a07594b0b0e2c
Edith Nqobile Muyambiri <nqobilemuyambiri@gmail.com>
2026-02-05 14:21:53 +0200

    new data

--- .next/dev/static/chunks/Takura-Bid_src_app_layout_tsx_0ff26aab._.js
+++ .next/dev/static/chunks/Takura-Bid_src_app_layout_tsx_0ff26aab._.js
@@ -1,7 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__057d1f6a._.css"
+  "static/chunks/[root-of-the-server]__057d1f6a._.css",
+  "static/chunks/Takura-Bid_8b6e1f36._.js"
 ],
     source: "dynamic"
 });
